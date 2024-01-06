53d16f415a5986672b997fd386e612d7325652ff
MohammedMazher1 <121592398+MohammedMazher1@users.noreply.github.com>
2024-01-06 21:32:20 +0300

    add new main layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,106 +1,205 @@
 <!DOCTYPE html>
 <html lang="en" dir="rtl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('/assets/css/normalize.css')}}">
-    <link rel="stylesheet" href="{{asset('/assets/css/all.min.css')}}">
-    <link rel="stylesheet" href="{{asset('/assets/css/main.css')}}">
-    <link rel="shortcut icon" href="{{asset('/assets/img/logo.png')}}">
-
+  <head>
+    <meta charset="utf-8" />
     <title>Tester</title>
-</head>
-<body>
-    <!-- start header  -->
-    <header class="header">
-        <div class="container">
-            <nav>
-                <div class="links">
-                    <img class="logo" src="assets/img/logo.png" alt="logo">
-                    <span id="headerLine"></span>
-                    <span class="fas fa-bars toggle-menu"></span>
-                    <ul>
-                        <li>
-                            <a href="#home" class="active">الرئسية</a>
-                        </li>
-                        <li>
-                            <a href="#services">الخدمات</a>
-                        </li>
-                        <li>
-                            <a href="#about-us">من نحن؟</a>
-                        </li>
-                        <li>
-                            <a href="#footer">تواصل معنا</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="inputs">
-                    <div class="search">
-                        <i class="fa fa-search"></i>
-                        <input type="text" class="headerInput" placeholder="اكتب كلمة للبحث .....">
-                    </div>
-                    <div class="login ">
-                        @guest
-                        <a  href="{{ route('login') }}" class="headerInput">
-                            <span>تسجيل الدخول</span>
-                            <i class="fa-solid fa-registered"></i>
-                        </a>
-                        @else
-                        <form id="logout" action="{{ route('logout') }}" method="POST">
-                            <a role="button" class="headerInput"
-                            onclick="document.getElementById('logout').submit();">تسجيل الخروج</a>
-                            @csrf
-                        </form>
-                        @endguest
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
 
-                    </div>
-                </div>
-            </nav>
+    <!-- Flaticon Font -->
+    <link href="{{asset('/assets/lib/flaticon/font/flaticon.css')}}" rel="stylesheet" />
+
+    <!-- Libraries Stylesheet -->
+    <link href="{{asset('/assets/lib/owlcarousel/assets/owl.carousel.min.css')}}" rel="stylesheet" />
+    <link href="{{asset('/assets/lib/lightbox/css/lightbox.min.css')}}" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="{{asset('/assets/css/style.css')}}" rel="stylesheet" />
+  </head>
+
+  <body>
+    <!-- Navbar Start -->
+    <div class="container-fluid bg-light position-relative shadow">
+      <nav
+        class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-0 px-lg-5"
+      >
+        <a
+          href=""
+          class="navbar-brand font-weight-bold text-secondary d-flex justify-content-center align-item-center"
+          style="font-size: 50px"
+        >
+            <img class="logo" src="img/logo.png" alt="">
+        </a>
+        <button
+          type="button"
+          class="navbar-toggler"
+          data-toggle="collapse"
+          data-target="#navbarCollapse"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div
+          class="collapse navbar-collapse justify-content-between"
+          id="navbarCollapse"
+        >
+          <div class="navbar-nav font-weight-bold mx-auto py-0">
+            <a href="index.html" class="nav-item nav-link active">الرئسية</a>
+            <a href="about.html" class="nav-item nav-link">من نحن ؟</a>
+            <a href="class.html" class="nav-item nav-link">خدماتنا</a>
+            <a href="class.html" class="nav-item nav-link">البرامج</a>
+            <a href="contact.html" class="nav-item nav-link">تواصل معنا</a>
+          </div>
+          <a href="" class="btn btn-primary px-5 ml-2" >الإدارة</a>
+          <div class="mx-3 text-primary">
+            <i class="fa fa-user"></i>
+            <span>محمد مزهر عمر </span>
+          </div>
+          <a href="" class="btn btn-primary px-4">تسجيل الدخول</a>
         </div>
-    </header>
-        <!-- end header  -->
+      </nav>
+    </div>
+    <!-- Navbar End -->
         <!-- start main -->
         <main>
             @yield('content')
         </main>
         <!-- end main -->
-        <!-- start footer  -->
-        <footer class="footer" id="footer">
-            <div class="related">
-                <div>
-                    <h3>مواقع مُشابهة</h3>
-                    <p>https://www.jotform.com/test-maker/</p>
-                    <p>https://www.typeform.com/test-maker/</p>
-                    <p>https://www.flexiquiz.com/</p>
-                </div>
-                <div>
-                    <img src="assets/img/logo.png" alt="">
-                    <span class="circule"></span>
-                </div>
+            <!-- Footer Start -->
+    <div class="container-fluid bg-primary text-white mt-5 py-5 px-sm-3 px-md-5">
+        <div class="row pt-5">
+          <div class="col-lg-3 col-md-6 mb-5 d-flex justify-content-center align-item-start flex-column">
+            <a href="" class="text-right">
+              <img src="img/whitelogo.png" class="w-75" alt="logo">
+            </a>
+            <p class="text-right">
+              عناويننا على مواقع التواصل لا تتردد في التواصل معنا والاستفسار سيتم الرد على استفساراتكم خلال 24 ساعة
+            </p>
+            <div class="d-flex justify-content-start mt-4">
+              <a
+                class="btn btn-outline-primary border rounded-circle text-center mr-2 px-0"
+                style="width: 38px; height: 38px"
+                href="#"
+                ><i class="fab fa-twitter"></i
+              ></a>
+              <a
+                class="btn btn-outline-primary border rounded-circle text-center mr-2 px-0"
+                style="width: 38px; height: 38px"
+                href="#"
+                ><i class="fab fa-facebook-f"></i
+              ></a>
+              <a
+                class="btn btn-outline-primary border rounded-circle text-center mr-2 px-0"
+                style="width: 38px; height: 38px"
+                href="#"
+                ><i class="fab fa-linkedin-in"></i
+              ></a>
+              <a
+                class="btn btn-outline-primary border rounded-circle text-center mr-2 px-0"
+                style="width: 38px; height: 38px"
+                href="#"
+                ><i class="fab fa-instagram"></i
+              ></a>
             </div>
-            <div class="icons">
-                <div>
-                    <i class="fa-brands fa-github"></i>
-                    <p>MohammedMazher1</p>
-                </div>
-                <div>
-                    <i class="fa-brands fa-instagram"></i>
-                    <p>m.din6y</p>
-                </div>
-                <div>
-                    <i class="fa-brands fa-whatsapp"></i>
-                    <p>77653387</p>
-                </div>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-5 text-center">
+            <h3 class="text-primary mb-4">نتواجد</h3>
+            <div class="d-flex justify-content-center">
+              <h4 class="fa fa-map-marker-alt text-primary"></h4>
+              <div class="pl-3">
+                <h5 class="text-white">العنوان</h5>
+                <p>المكلا - الشرج</p>
+               </div>
             </div>
-            <div class="copy">
-                Mohammed Mazher
-                &copy;
+            <div class="d-flex justify-content-center">
+              <div class="pl-3">
+                <h4 class="fa fa-envelope text-primary d-inline-block"></h4>
+                <h5 class="text-white d-inline-block">الأيميل</h5>
+                <p>muhmmed711300411@gmail.com</p>
+              </div>
             </div>
-        </footer>
-        <!-- end footer  -->
-        <script src="{{asset('/assets/lib/jquery-3.7.1.js')}}"></script>
-        <script src="{{asset('/assets/js/main.js')}}"></script>
-       
-</body>
-</html>
+            <div class="d-flex justify-content-center">
+              <h4 class="fa fa-phone-alt text-primary"></h4>
+              <div class="pl-3">
+                <h5 class="text-white">الهاتف</h5>
+                <p>+967 776533887</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-5">
+            <h3 class="text-primary mb-4 text-center">روابط سريعة</h3>
+            <div class="d-flex flex-column justify-content-center align-item-center">
+              <a class="d-flex justify-content-around text-white text-center mb-2 w-100" href="#">
+                الرئسية
+                <i class="fa fa-angle-right mr-2"></i>
+                </a>
+              <a class="d-flex justify-content-around text-white text-center mb-2" href="#">
+                من نحن ؟
+                <i class="fa fa-angle-right mr-2"></i>
+              </a>
+              <a class="d-flex justify-content-around text-white text-center mb-2" href="#">البرامج
+                <i class="fa fa-angle-right mr-2"></i>
+              </a>
+              <a class="d-flex justify-content-around text-white text-center mb-2" href="#">الخدمات
+                <i class="fa fa-angle-right mr-2"></i></a>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 mb-5">
+            <h3 class="text-primary mb-4 text-center">ارسل لنا</h3>
+            <form action="">
+              <div class="form-group">
+                <input
+                  type="text"
+                  class="form-control border-0 py-4"
+                  placeholder="اسمك "
+                  required="required"
+                />
+              </div>
+              <div class="form-group">
+                <input
+                  type="email"
+                  class="form-control border-0 py-4"
+                  placeholder="الايميل "
+                  required="required"
+                />
+              </div>
+              <div>
+                <button
+                  class="btn btn-primary border btn-block py-3"
+                  type="submit"
+                >
+                  ارسال
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div
+          class="container-fluid pt-5"
+          style="border-top: 1px solid rgba(23, 162, 184, 0.2) ;"
+        >
+          <p class="m-0 text-center text-white">
+            &copy;
+            <a class="text-primary font-weight-bold" href="https:\\mohammedmazher1.github.io\aboutme">About me</a>.
+            <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+            Designed by
+            <a class="text-primary font-weight-bold" href="https:\\mohammedmazher1.github.io\aboutme"
+              >Mohammed Mazher</a>
+          </p>
+        </div>
+      </div>
+      <!-- Footer End -->
+      <!-- Back to Top -->
+      <a href="#" class="btn btn-primary p-3 back-to-top border"
+        ><i class="fa fa-angle-double-up"></i
+      ></a>
+
+      <!-- JavaScript Libraries -->
+      <script src="{{asset('/assets/lib/easing/easing.min.js')}}"></script>
+      <script src="{{asset('/assets/lib/owlcarousel/owl.carousel.min.js')}}"></script>
+      <script src="{{asset('/assets/lib/isotope/isotope.pkgd.min.js')}}"></script>
+      <script src="{{asset('/assets/lib/lightbox/js/lightbox.min.js')}}"></script>
+
+
+      <!-- Template Javascript -->
+      <script src="{{asset('assets/js/main.js')}}"></script>
+    </body>
+  </html>
