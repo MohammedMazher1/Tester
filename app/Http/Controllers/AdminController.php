06e8b7c1ba2a00bb91f79061ca47f505e3202640
MohammedMazher1 <121592398+MohammedMazher1@users.noreply.github.com>
2024-01-10 00:09:17 +0300

    question page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,6 +9,6 @@ class AdminController extends Controller
 {
     public function index(){
         $exams = Exam::all();
-        return $exams;
+        return view('admin.examList',compact('exams'));
     }
 }
--- resources/views/admin/examList.blade.php
+++ resources/views/admin/examList.blade.php
@@ -1,19 +1,17 @@
 @extends('layouts.admin')
 @section('content')
-<div class="container-fluid py-5">
-    <div class="container row row-cols-1 row-cols-md-4 mx-auto">
+    <div class="examlist">
         @foreach ($exams as $exam)
-        <div class="col shadow p-3 text-center">
+        <div class="">
             <i class="fa-solid fa-question"></i>
             <h4>{{$exam->name}}</h4>
-            <span>20 question</span>
+            <span>question {{$exam->questions->count()}}</span>
             <i class="fa-solid fa-star text-primary "></i>
             <i class="fa-solid fa-star text-primary"></i>
             <i class="fa-solid fa-star text-primary"></i>
             <i class="fa-solid fa-star text-primary"></i>
         </div>
         @endforeach
-
     </div>
- </div>
-@endsection
+ @endsection
+
